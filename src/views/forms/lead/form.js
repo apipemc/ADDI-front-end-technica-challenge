8993b9945f10d8f75e76bffd8f38f5039a2fb369
Andres Felipe Martinez Cordero <andrepipemc@gmail.com>
2019-06-30 18:57:14 -0500

    Add style forms

--- src/views/forms/lead/form.js
+++ src/views/forms/lead/form.js
@@ -76,128 +76,187 @@ const ContactsForm = ({
 }) => (
   <Form onSubmit={handleSubmit}>
     <Hidden name="id" />
-    <div>
-      <label htmlFor="first_name">
+    <div className="field">
+      <label className="label" htmlFor="first_name">
         First Name:
+      </label>
+      <div className="control-input">
         <Text
           id="first_name"
           name="first_name"
           placeholder="First Name"
           validate={[required()]}
         />
+      </div>
+    </div>
+    <div className="field">
+      <label className="label" htmlFor="last_name">
+        Last Name:{' '}
       </label>
+      <div className="control-input">
+        <Text
+          id="last_name"
+          name="last_name"
+          placeholder="Last Name"
+          validate={[required()]}
+        />
+      </div>
     </div>
-    <div>
-      <label htmlFor="last_name">Last Name: </label>
-      <Text
-        id="last_name"
-        name="last_name"
-        placeholder="Last Name"
-        validate={[required()]}
-      />
+    <div className="field">
+      <label className="label" htmlFor="document_type">
+        Document Type:{' '}
+      </label>
+      <div className="control-input">
+        <div className="select">
+          <Select
+            id="document_type"
+            name="document_type"
+            options={DocumentTypeOptions}
+            validate={[required()]}
+          />
+        </div>
+      </div>
     </div>
-    <div>
-      <label htmlFor="document_type">Document Type: </label>
-      <Select
-        id="document_type"
-        name="document_type"
-        options={DocumentTypeOptions}
-        validate={[required()]}
-      />
+    <div className="field">
+      <label className="label" htmlFor="email">
+        Document:{' '}
+      </label>
+      <div className="control-input">
+        <Text
+          name="document_id"
+          placeholder="Document"
+          validate={[required(), length({ max: 12 })]}
+        />
+      </div>
     </div>
-    <div>
-      <label htmlFor="email">Document: </label>
-      <Text
-        name="document_id"
-        placeholder="Document"
-        validate={[required(), length({ max: 12 })]}
-      />
+    <div className="field">
+      <label className="label" htmlFor="date_of_expedition">
+        Date of Expedition:{' '}
+      </label>
+      <div className="control-input">
+        <DatePicker
+          id="date_of_expedition"
+          name="date_of_expedition"
+          placeholder="Date of Expedition"
+          max={maxDate()}
+          validate={[
+            required(),
+            date({
+              format: 'mm/dd/yyyy',
+            }),
+          ]}
+        />
+      </div>
     </div>
-    <div>
-      <label htmlFor="date_of_expedition">Date of Expedition: </label>
-      <DatePicker
-        id="date_of_expedition"
-        name="date_of_expedition"
-        placeholder="Date of Expedition"
-        max={maxDate()}
-        validate={[
-          required(),
-          date({
-            format: 'mm/dd/yyyy',
-          }),
-        ]}
-      />
+    <div className="field">
+      <label className="label" htmlFor="email">
+        Email:
+      </label>
+      <div className="control-input">
+        <Text
+          id="email"
+          name="email"
+          type="email"
+          placeholder="Email"
+          validate={[required(), email()]}
+        />
+      </div>
     </div>
-    <div>
-      <label htmlFor="email">Email:</label>
-      <Text
-        id="email"
-        name="email"
-        type="email"
-        placeholder="Email"
-        validate={[required(), email()]}
-      />
+    <div className="field">
+      <label className="label" htmlFor="date_of_birth">
+        Date of Birthday:{' '}
+      </label>
+      <div className="control-input">
+        <DatePicker
+          id="date_of_birthday"
+          name="date_of_birthday"
+          placeholder="Date of Birthday"
+          max={maxDate()}
+          validate={[
+            required(),
+            date({
+              format: 'mm/dd/yyyy',
+              '<=': eighTeenYearsAgo,
+              msg: 'you must be at least 18 years old',
+            }),
+          ]}
+        />
+      </div>
     </div>
-    <div>
-      <label htmlFor="date_of_birth">Date of Birthday: </label>
-      <DatePicker
-        id="date_of_birthday"
-        name="date_of_birthday"
-        placeholder="Date of Birthday"
-        max={maxDate()}
-        validate={[
-          required(),
-          date({
-            format: 'mm/dd/yyyy',
-            '<=': eighTeenYearsAgo,
-            msg: 'you must be at least 18 years old',
-          }),
-        ]}
-      />
+    <div className="field">
+      <label className="label" htmlFor="gender">
+        Gender:{' '}
+      </label>
+      <div className="control-input">
+        <div className="select">
+          <Select
+            id="gender"
+            name="gender"
+            options={GenderOptions}
+            validate={[required()]}
+          />
+        </div>
+      </div>
     </div>
-    <div>
-      <label htmlFor="gender">Gender: </label>
-      <Select
-        id="gender"
-        name="gender"
-        options={GenderOptions}
-        validate={[required()]}
-      />
+    <div className="field">
+      <label className="label" htmlFor="address">
+        Address:{' '}
+      </label>
+      <div className="control-input">
+        <Text id="address" name="address" placeholder="Address" />
+      </div>
     </div>
-    <div>
-      <label htmlFor="address">Address: </label>
-      <Text id="address" name="address" placeholder="Address" />
+    <div className="field">
+      <label className="label" htmlFor="neighborhood">
+        Neighborhood:{' '}
+      </label>
+      <div className="control-input">
+        <Text
+          id="neighborhood"
+          name="neighborhood"
+          placeholder="Neighborhood"
+        />
+      </div>
     </div>
-    <div>
-      <label htmlFor="neighborhood">Neighborhood: </label>
-      <Text id="neighborhood" name="neighborhood" placeholder="Neighborhood" />
+    <div className="field">
+      <label className="label" htmlFor="phone_number">
+        Phone Number:{' '}
+      </label>
+      <div className="control-input">
+        <Text
+          id="phone_number"
+          name="phone_number"
+          type="tel"
+          placeholder="Phone Number"
+          validate={[numericality({ allowBlank: true }), length({ max: 11 })]}
+        />
+      </div>
     </div>
-    <div>
-      <label htmlFor="phone_number">Phone Number: </label>
-      <Text
-        id="phone_number"
-        name="phone_number"
-        type="tel"
-        placeholder="Phone Number"
-        // validate={[numericality({ allowBlank: true}), length({ max: 11 })]}
-      />
+    <div className="field">
+      <label className="label" htmlFor="mobile_number">
+        Mobile Number:{' '}
+      </label>
+      <div className="control-input">
+        <Text
+          id="mobile_number"
+          name="mobile_number"
+          type="tel"
+          placeholder="Mobile Number"
+          validate={[numericality({ allowBlank: true }), length({ max: 11 })]}
+        />
+      </div>
     </div>
-    <div>
-      <label htmlFor="mobile_number">Mobile Number: </label>
-      <Text
-        id="mobile_number"
-        name="mobile_number"
-        type="tel"
-        placeholder="Mobile Number"
-        validate={[numericality({ allowBlank: true }), length({ max: 11 })]}
-      />
+    <div className="field">
+      <div className="control-input">
+        <button
+          className="button success is-fullwidth"
+          type="submit"
+          disabled={pristine || submitting || (submitFailed ? false : invalid)}
+        >
+          {initialValues.has('id') ? 'Update' : 'Create'}
+        </button>
+      </div>
     </div>
-    <button
-      type="submit"
-      disabled={pristine || submitting || (submitFailed ? false : invalid)}
-    >
-      {initialValues.has('id') ? 'Update' : 'Create'}
-    </button>
   </Form>
 );
 
